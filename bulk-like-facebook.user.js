aab9805ad234f98a2d39f81ac3ef94604b3996e8
phoeagon <phoeagon@gmail.com>
2013-08-13 18:40:04 +0800

    fb

--- bulk-like-facebook.user.js
+++ bulk-like-facebook.user.js
@@ -2,13 +2,12 @@
 // @name          Bulk Like on Xiaonei
 // @namespace     http://about.me/phoeagon
 // @description   Bulk like every item on your timeline
-// @include       *://*.renren.com/*
-// @include       *://*.renren.com/
-// @include       *://renren.com/*
-// @include       *://renren.com/
-// @exclude       *://*.renren.com/ajaxproxy.html*
-// @updateURL   https://github.com/phoeagon/bulk-like-sns-userscript/raw/master/bulk-like-xiaonei.user.js
-// @downloadURL   https://github.com/phoeagon/bulk-like-sns-userscript/raw/master/bulk-like-xiaonei.user.js
+// @include       *://*.facebook.com/*
+// @include       *://*.facebook.com/
+// @include       *://facebook.com/*
+// @include       *://facebook.com/
+// @updateURL   https://github.com/phoeagon/bulk-like-sns-userscript/raw/master/bulk-like-facebook.user.js
+// @downloadURL   https://github.com/phoeagon/bulk-like-sns-userscript/raw/master/bulk-like-facebook.user.js
 // ==/UserScript==
 
 /*
@@ -30,13 +29,13 @@ function callback() {
     if (!window)return;
      //       alert("fuck")
       //  console.log( " JQUERY loaded" )
-       jQ('.nav-main').append(
-            jQ('<div>').addClass('menu').append(
-                jQ('<div>').addClass('menu-title').append(
-                    jQ('<a>').attr('href','#').click(function(){
-                            alert("Bulk-like-xiaonei,\tby phoeagon\n2013, Aug 13七姐誕版\n點讚過程已開始")
-                            jQ('.ilike_icon').each( function ( ind , ele ){
-                                if ( jQ(ele).html().indexOf("取消")==-1 ){
+       jQ('#pageNav').append(
+            jQ('<li>').addClass('navItem').append(
+                    jQ('<a>').attr('href','#').addClass('navLink bigPadding')
+                    .click(function(){
+                            alert("Bulk-like-facebook,\tby phoeagon\n2013, Aug 13七姐誕版\n點讚過程已開始")
+                            jQ('.UFILikeLink').each( function ( ind , ele ){
+                                if ( jQ(ele).html().indexOf("Unlike")==-1 ){
                                     setTimeout( function(){
                                         jQ(ele).click();
                                         console.log(ind);
@@ -44,7 +43,6 @@ function callback() {
                                 }
                             })
                         }).html('點讚')
-                )
        ))
 
 }
