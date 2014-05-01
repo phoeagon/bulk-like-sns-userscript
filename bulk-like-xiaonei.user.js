ff233f45b73a4c90d5ef12774a639a1367d4a6e0
Qiu Zhe <phoeagon@gmail.com>
2014-05-01 15:15:48 +0800

    changes

--- bulk-like-xiaonei.user.js
+++ bulk-like-xiaonei.user.js
@@ -26,16 +26,44 @@ function addJQuery(callback) {
     }, false);
     document.body.appendChild(script);
 }
+
 addJQuery(callback)
+
 function callback() {
     if (!window)return;
      //       alert("fuck")
       //  console.log( " JQUERY loaded" )
+      if (jQ('html').hasClass('nx-main760')){
+		  jQ('.app-nav-list').append(
+			jQ('<li>').addClass('app-nav-item').attr('data-tip','點讚')
+			.append(
+				jQ('<a>').attr('href','#').css('font-size','200%').html('點讚').click( function (){
+                            alert("Bulk-like-xiaonei,\tby phoeagon\n2014, Apr 24GRE前作死版\n點讚過程已開始")
+                            jQ('.like').each( function ( ind , ele ){
+								try{
+                                if (! jQ(ele).hasClass('liked') ){
+                                    setTimeout( function(){
+										console.log(ele)
+                                        jQ(ele)[0].mouseover();
+                                        jQ(ele)[0].click();
+                                        console.log(ind);
+                                    }  , ind*2000 );
+                                }
+								}catch(err){
+									console.log(err)
+								}
+                            })
+					} )
+			)
+		  )
+	  }else{
+	// old version
        jQ('.nav-main').append(
             jQ('<div>').addClass('menu').append(
                 jQ('<div>').addClass('menu-title').append(
-                    jQ('<a>').attr('href','#').click(function(){
-                            alert("Bulk-like-xiaonei,\tby phoeagon\n2013, Aug 13七姐誕版\n點讚過程已開始")
+                    jQ('<a>').attr('href','#').click( 
+						function (){
+                            alert("Bulk-like-xiaonei,\tby phoeagon\n2014, Apr 24GRE前作死版\n點讚過程已開始")
                             jQ('.ilike_icon').each( function ( ind , ele ){
                                 if ( jQ(ele).html().indexOf("取消")==-1 ){
                                     setTimeout( function(){
@@ -44,13 +72,15 @@ function callback() {
                                     }  , ind*2000 );
                                 }
                             })
-                        }).html('點讚')
+                        } ).html('點讚')
                 )
        ))
+	  }
 
 }
 //------------------------------------------------------------------
 /*
  * Changelog:
  *  2013 Aug 13 Initial Deployable
+ * 2014 Apr 24  fit V7
  * */
